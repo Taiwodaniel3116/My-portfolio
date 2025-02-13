111eb0a339ae22db11bb3786b9cb3b177408c665
TaiwoDaniel <taiwodaniel3116@gmail.com>
2025-02-13 04:48:57 -0800

    add new changes, pctures, animations and stylings

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ hamburgerMenu.addEventListener('click', () => {
   navContainer.classList.toggle('active');
   hamburgerMenu.classList.toggle('active');
 });
+
+// ANIMATION
+const sphere = document.getElementById('sphere');
+const rows = []
+for (let i = 0; i < 180; i += 7) {
+   rows.push(`<div class ="line" style= "transform: rotateY(${i}deg);"></div>`);
+}
+
+sphere.innerHTML = rows.join("");
